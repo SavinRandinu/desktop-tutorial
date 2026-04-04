ff97430af422ec7463878d29a0adb23887dcece7
SavinMadapatha <madapathasavin@gmail.com>
2026-04-04 13:22:11 +0530

    initial session true false based Navbar

--- my-next-app/src/app/_components/Navbar.tsx
+++ my-next-app/src/app/_components/Navbar.tsx
@@ -1,14 +1,38 @@
 import Link from 'next/link';
 import React from 'react';
+import LoginOutButton from './LoginOutButton';
 
 const Navbar = () => {
-    return <nav className="bg-white shadow-sm">
-        <div className="container mx-auto px-4 py-4 flex items-center">
-            <Link href="/" className="text-xl font-bold text-blue-600">
-                Contact Manager
-            </Link>
-        </div>
-    </nav>;
+    const session = true;
+    return (
+        <nav className="bg-white shadow-sm">
+            <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+                <Link href="/" className="text-xl font-bold text-blue-600">
+                    Contact Manager
+                </Link>
+                <div className="flex items-center space-x-4">
+                    {session ? (
+                        <>
+                            <Link href="/contact" className="hover:text-blue-600 mr-8">
+                                Contacts
+                            </Link>
+                            <LoginOutButton />
+                        </>
+                    ) : (
+                        <>
+                            <Link href="/login" className="hover:text-blue-600 mr-5">
+                                Login
+                            </Link>
+                            <Link href="/register" className="hover:text-blue-600">
+                                Register
+                            </Link>
+                        </>
+
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
